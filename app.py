c5b7802db16f273c70ac2312897331ec972ac0e5
analizbilisim9 <analizbilisim9@gmail.com>
2026-05-07 12:05:26 +0300

    Move init_db call to after fetching emails

--- app.py
+++ app.py
@@ -106,7 +106,8 @@ def emails():
     conn.close()
     return jsonify([dict(r) for r in rows])
 
+init_db()
+
 if __name__ == "__main__":
-    init_db()
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
